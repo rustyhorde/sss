3c03d5076d502f963f89e08994282bd69805c3ec
Jason Ozias <jason.ozias@gmail.com>
2020-11-18 01:51:29 -0500

    converted to library

---
+++ src/gf256/mod.rs
@@ -0,0 +1,199 @@
+// Copyright (c) 2020 sss developers
+//
+// Licensed under the Apache License, Version 2.0
+// <LICENSE-APACHE or http://www.apache.org/licenses/LICENSE-2.0> or the MIT
+// license <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. All files in the project carrying such notice may not be copied,
+// modified, or distributed except according to those terms.
+
+//! `sss` GF(2^8)
+
+mod constants;
+
+use constants::{EXP, LOG};
+use rand::Rng;
+
+crate fn generate(d: usize, x: u8) -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    let mut rand_bytes: Vec<u8>;
+
+    loop {
+        rand_bytes = (0..d).map(|_| rng.gen::<u8>()).collect();
+        if degree(&rand_bytes) == (d - 1) {
+            break;
+        }
+    }
+
+    rand_bytes[0] = x;
+    rand_bytes
+}
+
+crate fn eval(p: &[u8], x: u8) -> u8 {
+    let mut result = 0;
+
+    for i in (0..=(p.len() - 1)).rev() {
+        result = add(mul(result, x), p[i])
+    }
+    result
+}
+
+crate fn interpolate(points: Vec<Vec<u8>>) -> u8 {
+    let x = 0;
+    let mut y = 0;
+
+    for i in 0..points.len() {
+        let a_x = points[i][0];
+        let a_y = points[i][1];
+        let mut li = 1;
+
+        for j in 0..points.len() {
+            let b_x = points[j][0];
+            if i != j {
+                li = mul(li, div(sub(x, b_x), sub(a_x, b_x)));
+            }
+        }
+        y = add(y, mul(li, a_y));
+    }
+
+    y
+}
+
+fn degree(p: &[u8]) -> usize {
+    for i in (1..=(p.len() - 1)).rev() {
+        if p[i] != 0 {
+            return i;
+        }
+    }
+    0
+}
+
+fn mul(a: u8, b: u8) -> u8 {
+    if a == 0 || b == 0 {
+        0
+    } else {
+        let a = usize::from(a);
+        let b = usize::from(b);
+        let left = usize::from(LOG[a]);
+        let right = usize::from(LOG[b]);
+        EXP[left + right]
+    }
+}
+
+fn div(a: u8, b: u8) -> u8 {
+    mul(a, EXP[255usize - usize::from(LOG[usize::from(b)])])
+}
+
+fn add(a: u8, b: u8) -> u8 {
+    a ^ b
+}
+
+fn sub(a: u8, b: u8) -> u8 {
+    add(a, b)
+}
+
+#[cfg(test)]
+mod test {
+    use super::{add, degree, div, eval, generate, interpolate, mul, sub};
+
+    #[test]
+    fn add_works() {
+        assert_eq!(122, add(100, 30));
+    }
+
+    #[test]
+    fn sub_works() {
+        assert_eq!(122, sub(100, 30));
+    }
+
+    #[test]
+    fn mul_works() {
+        assert_eq!(167, mul(133, 5));
+        assert_eq!(254, mul(90, 21));
+        assert_eq!(0, mul(0, 21));
+        assert_eq!(0x36, mul(0xb6, 0x53));
+    }
+
+    #[test]
+    fn div_works() {
+        assert_eq!(189, div(90, 21));
+        assert_eq!(151, div(6, 55));
+        assert_eq!(138, div(22, 192));
+        assert_eq!(0, div(0, 192));
+    }
+
+    #[test]
+    fn mul_is_commutative() {
+        for i in 0..255 {
+            for j in 0..255 {
+                assert_eq!(mul(i, j), mul(j, i));
+            }
+        }
+    }
+
+    #[test]
+    fn add_is_commutative() {
+        for i in 0..255 {
+            for j in 0..255 {
+                assert_eq!(add(i, j), add(j, i));
+            }
+        }
+    }
+
+    #[test]
+    fn sub_is_inverse_of_add() {
+        for i in 0..255 {
+            for j in 0..255 {
+                assert_eq!(sub(add(i, j), j), i);
+            }
+        }
+    }
+
+    #[test]
+    fn div_is_inverse_of_mul() {
+        for i in 0..255 {
+            for j in 1..255 {
+                assert_eq!(div(mul(i, j), j), i);
+            }
+        }
+    }
+
+    #[test]
+    fn mul_is_inverse_of_div() {
+        for i in 0..255 {
+            for j in 1..255 {
+                assert_eq!(mul(div(i, j), j), i);
+            }
+        }
+    }
+
+    #[test]
+    fn degree_works() {
+        assert_eq!(degree(&vec![1, 2]), 1);
+        assert_eq!(degree(&vec![1, 2, 0]), 1);
+        assert_eq!(degree(&vec![1, 2, 3]), 2);
+        assert_eq!(degree(&vec![4]), 0);
+    }
+
+    #[test]
+    fn eval_works() {
+        assert_eq!(eval(&vec![1, 0, 2, 3], 2), 17);
+    }
+
+    #[test]
+    fn generate_works() {
+        let p = generate(5, 20);
+        assert_eq!(p[0], 20);
+        // assert_eq!(p.len(), 6);
+        assert!(p[p.len() - 1] != 0);
+    }
+
+    #[test]
+    fn interpolate_works() {
+        assert_eq!(interpolate(vec![vec![1, 1], vec![2, 2], vec![3, 3]]), 0);
+        assert_eq!(interpolate(vec![vec![1, 80], vec![2, 90], vec![3, 20]]), 30);
+        assert_eq!(
+            interpolate(vec![vec![1, 43], vec![2, 22], vec![3, 86]]),
+            107
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -8,63 +8,163 @@
 
 //! `sss`
 
-mod constants;
+#![feature(crate_visibility_modifier, error_iter)]
+#![deny(
+    absolute_paths_not_starting_with_crate,
+    anonymous_parameters,
+    array_into_iter,
+    asm_sub_register,
+    bare_trait_objects,
+    bindings_with_variant_name,
+    box_pointers,
+    broken_intra_doc_links,
+    cenum_impl_drop_cast,
+    clashing_extern_declarations,
+    coherence_leak_check,
+    confusable_idents,
+    const_evaluatable_unchecked,
+    const_item_mutation,
+    dead_code,
+    deprecated,
+    deprecated_in_future,
+    drop_bounds,
+    elided_lifetimes_in_paths,
+    ellipsis_inclusive_range_patterns,
+    explicit_outlives_requirements,
+    exported_private_dependencies,
+    illegal_floating_point_literal_pattern,
+    improper_ctypes,
+    improper_ctypes_definitions,
+    incomplete_features,
+    indirect_structural_match,
+    inline_no_sanitize,
+    invalid_codeblock_attributes,
+    invalid_html_tags,
+    invalid_value,
+    irrefutable_let_patterns,
+    keyword_idents,
+    late_bound_lifetime_arguments,
+    macro_use_extern_crate,
+    meta_variable_misuse,
+    missing_copy_implementations,
+    missing_crate_level_docs,
+    missing_debug_implementations,
+    missing_doc_code_examples,
+    missing_docs,
+    mixed_script_confusables,
+    mutable_borrow_reservation_conflict,
+    no_mangle_generic_items,
+    non_ascii_idents,
+    non_camel_case_types,
+    non_shorthand_field_patterns,
+    non_snake_case,
+    non_upper_case_globals,
+    nontrivial_structural_match,
+    overlapping_patterns,
+    path_statements,
+    pointer_structural_match,
+    private_doc_tests,
+    private_in_public,
+    proc_macro_derive_resolution_fallback,
+    redundant_semicolons,
+    renamed_and_removed_lints,
+    safe_packed_borrows,
+    single_use_lifetimes,
+    stable_features,
+    trivial_bounds,
+    trivial_casts,
+    trivial_numeric_casts,
+    type_alias_bounds,
+    tyvar_behind_raw_pointer,
+    unaligned_references,
+    uncommon_codepoints,
+    unconditional_recursion,
+    unknown_lints,
+    unnameable_test_items,
+    unreachable_code,
+    unreachable_patterns,
+    unreachable_pub,
+    unsafe_code,
+    // unsafe_op_in_unsafe_fn,
+    // unstable_features,
+    unstable_name_collisions,
+    unused_allocation,
+    unused_assignments,
+    unused_attributes,
+    unused_braces,
+    unused_comparisons,
+    unused_crate_dependencies,
+    unused_doc_comments,
+    unused_extern_crates,
+    unused_features,
+    unused_import_braces,
+    unused_imports,
+    unused_labels,
+    unused_lifetimes,
+    unused_macros,
+    unused_must_use,
+    unused_mut,
+    unused_parens,
+    unused_qualifications,
+    unused_results,
+    unused_unsafe,
+    unused_variables,
+    variant_size_differences,
+    where_clauses_object_safety,
+    while_true,
+)]
 
-use constants::{EXP, LOG};
-use rand::Rng;
+mod gf256;
+
 use std::collections::HashMap;
 
+/// Make some shares
 pub fn split(secret: &[u8], parts: usize, threshold: usize) -> HashMap<usize, Vec<u8>> {
-    let mut values: Vec<Vec<u8>> = vec![vec![0; secret.len()]; parts];
+    let coeff_fn = |secret_byte: &u8| -> Vec<u8> { gf256::generate(threshold, *secret_byte) };
+    let gf_add =
+        |p: Vec<u8>| -> Vec<u8> { (1..=parts).map(|i| gf256::eval(&p, i as u8)).collect() };
 
-    let _: Vec<()> = secret
+    transpose(secret.iter().map(coeff_fn).map(gf_add).collect())
         .iter()
+        .cloned()
         .enumerate()
-        .map(|(idx, secret_byte)| {
-            let p = generate(threshold, *secret_byte);
-            for i in 1..=parts {
-                values[i - 1][idx] = eval(&p, i as u8);
-            }
-        })
-        .collect();
-
-    let mut shares: HashMap<usize, Vec<u8>> = HashMap::new();
-    for (idx, value) in values.iter().enumerate() {
-        let _ = shares.insert(idx + 1, value.clone());
-    }
-    shares
+        .map(inc_key)
+        .collect()
 }
 
-fn generate(d: usize, x: u8) -> Vec<u8> {
-    let mut rng = rand::thread_rng();
-    let mut rand_bytes: Vec<u8>;
-
-    loop {
-        rand_bytes = (0..d).map(|_| rng.gen::<u8>()).collect();
-        if degree(&rand_bytes) == (d - 1) {
-            break;
-        }
-    }
-
-    rand_bytes[0] = x;
-    rand_bytes
+fn inc_key(tuple: (usize, Vec<u8>)) -> (usize, Vec<u8>) {
+    (tuple.0 + 1, tuple.1)
 }
 
-pub fn join(len: usize, shares: HashMap<usize, Vec<u8>>) -> Vec<u8> {
+fn transpose<T>(v: Vec<Vec<T>>) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
+    if let Some(first) = v.get(0) {
+        (0..first.len())
+            .map(|i| v.iter().map(|inner| inner[i].clone()).collect::<Vec<T>>())
+            .collect()
+    } else {
+        vec![]
+    }
+}
+
+/// Join some shares
+pub fn join(shares: &HashMap<usize, Vec<u8>>) -> Vec<u8> {
     if !shares.is_empty() {
         let lengths: Vec<usize> = shares.values().map(Vec::len).collect();
-        println!("Lengths: {:?}", lengths);
+        let len = lengths[0];
         let mut secret = vec![];
         if lengths.iter().all(|x| *x == len) {
             for i in 0..lengths[0] {
                 let mut points = vec![vec![0; 2]; shares.len()];
                 let mut j = 0;
-                for (k, v) in &shares {
+                for (k, v) in shares {
                     points[j][0] = *k as u8;
                     points[j][1] = v[i];
                     j += 1;
                 }
-                secret.push(interpolate(points));
+                secret.push(gf256::interpolate(points));
             }
         }
 
@@ -75,193 +175,46 @@ pub fn join(len: usize, shares: HashMap<usize, Vec<u8>>) -> Vec<u8> {
     }
 }
 
-fn degree(p: &[u8]) -> usize {
-    for i in (1..=(p.len() - 1)).rev() {
-        if p[i] != 0 {
-            return i;
-        }
-    }
-    0
-}
-
-fn eval(p: &[u8], x: u8) -> u8 {
-    let mut result = 0;
-
-    for i in (0..=(p.len() - 1)).rev() {
-        result = add(mul(result, x), p[i])
-    }
-    result
-}
-
-fn mul(a: u8, b: u8) -> u8 {
-    if a == 0 || b == 0 {
-        0
-    } else {
-        let a = usize::from(a);
-        let b = usize::from(b);
-        let left = usize::from(LOG[a]);
-        let right = usize::from(LOG[b]);
-        EXP[left + right]
-    }
-}
-
-fn div(a: u8, b: u8) -> u8 {
-    mul(a, EXP[255usize - usize::from(LOG[usize::from(b)])])
-}
-
-fn add(a: u8, b: u8) -> u8 {
-    a ^ b
-}
-
-fn sub(a: u8, b: u8) -> u8 {
-    add(a, b)
-}
-
-fn interpolate(points: Vec<Vec<u8>>) -> u8 {
-    let x = 0;
-    let mut y = 0;
-
-    for i in 0..points.len() {
-        let a_x = points[i][0];
-        let a_y = points[i][1];
-        let mut li = 1;
-
-        for j in 0..points.len() {
-            let b_x = points[j][0];
-            if i != j {
-                li = mul(li, div(sub(x, b_x), sub(a_x, b_x)));
-            }
-        }
-        y = add(y, mul(li, a_y));
-    }
-
-    y
-}
-
 #[cfg(test)]
 mod test {
-    use super::{add, degree, div, eval, generate, interpolate, join, mul, split, sub};
+    use super::{join, split};
+    use rand::{rngs::ThreadRng, seq::IteratorRandom, thread_rng};
+    use std::collections::HashMap;
 
     #[test]
-    fn add_works() {
-        assert_eq!(122, add(100, 30));
-    }
-
-    #[test]
-    fn sub_works() {
-        assert_eq!(122, sub(100, 30));
-    }
-
-    #[test]
-    fn mul_works() {
-        assert_eq!(167, mul(133, 5));
-        assert_eq!(254, mul(90, 21));
-        assert_eq!(0, mul(0, 21));
-        assert_eq!(0x36, mul(0xb6, 0x53));
-    }
-
-    #[test]
-    fn div_works() {
-        assert_eq!(189, div(90, 21));
-        assert_eq!(151, div(6, 55));
-        assert_eq!(138, div(22, 192));
-        assert_eq!(0, div(0, 192));
-    }
-
-    #[test]
-    fn mul_is_commutative() {
-        for i in 0..255 {
-            for j in 0..255 {
-                assert_eq!(mul(i, j), mul(j, i));
-            }
-        }
-    }
-
-    #[test]
-    fn add_is_commutative() {
-        for i in 0..255 {
-            for j in 0..255 {
-                assert_eq!(add(i, j), add(j, i));
-            }
-        }
-    }
-
-    #[test]
-    fn sub_is_inverse_of_add() {
-        for i in 0..255 {
-            for j in 0..255 {
-                assert_eq!(sub(add(i, j), j), i);
-            }
-        }
-    }
-
-    #[test]
-    fn div_is_inverse_of_mul() {
-        for i in 0..255 {
-            for j in 1..255 {
-                assert_eq!(div(mul(i, j), j), i);
-            }
-        }
-    }
-
-    #[test]
-    fn mul_is_inverse_of_div() {
-        for i in 0..255 {
-            for j in 1..255 {
-                assert_eq!(mul(div(i, j), j), i);
-            }
-        }
-    }
-
-    #[test]
-    fn degree_works() {
-        assert_eq!(degree(&vec![1, 2]), 1);
-        assert_eq!(degree(&vec![1, 2, 0]), 1);
-        assert_eq!(degree(&vec![1, 2, 3]), 2);
-        assert_eq!(degree(&vec![4]), 0);
-    }
-
-    #[test]
-    fn eval_works() {
-        assert_eq!(eval(&vec![1, 0, 2, 3], 2), 17);
-    }
-
-    #[test]
-    fn generate_works() {
-        let p = generate(5, 20);
-        assert_eq!(p[0], 20);
-        // assert_eq!(p.len(), 6);
-        assert!(p[p.len() - 1] != 0);
-    }
-
-    #[test]
-    fn interpolate_works() {
-        assert_eq!(interpolate(vec![vec![1, 1], vec![2, 2], vec![3, 3]]), 0);
-        assert_eq!(interpolate(vec![vec![1, 80], vec![2, 90], vec![3, 20]]), 30);
-        assert_eq!(
-            interpolate(vec![vec![1, 43], vec![2, 22], vec![3, 86]]),
-            107
-        );
-    }
-
-    #[test]
-    fn blah() {
-        let secret = "fudge factory".as_bytes();
+    fn split_and_join() {
+        let secret = "correct horst battery staple".as_bytes();
         let shares = split(&secret, 5, 3);
-        println!("##### Shares");
-        for (key, value) in &shares {
-            println!("Key {}: {:?}", key, value);
-        }
 
+        // 5 parts should work
         let mut parts = shares.clone();
-        parts.remove(&1);
-        parts.remove(&5);
-        println!();
-        println!("##### Parts");
-        for (key, value) in &parts {
-            println!("Key {}: {:?}", key, value);
-        }
+        print_parts(&parts);
+        assert_eq!(join(&parts), secret);
 
-        assert_eq!(join(secret.len(), parts), secret);
+        // 4 parts shoud work
+        let mut rng = thread_rng();
+        let _ = choose_idx(&mut rng, &parts).and_then(|idx| parts.remove(&idx));
+        print_parts(&parts);
+        assert_eq!(join(&parts), secret);
+
+        // 3 parts should work
+        let _ = choose_idx(&mut rng, &parts).and_then(|idx| parts.remove(&idx));
+        print_parts(&parts);
+        assert_eq!(join(&parts), secret);
+
+        // 2 parts should not
+        let _ = choose_idx(&mut rng, &parts).and_then(|idx| parts.remove(&idx));
+        print_parts(&parts);
+        assert_ne!(join(&parts), secret);
+    }
+
+    fn choose_idx(rng: &mut ThreadRng, map: &HashMap<usize, Vec<u8>>) -> Option<usize> {
+        map.clone().keys().choose(rng).cloned()
+    }
+
+    fn print_parts(map: &HashMap<usize, Vec<u8>>) {
+        for (k, v) in map {
+            println!("Key: {}, Value: {:?}", k, v);
+        }
     }
 }
